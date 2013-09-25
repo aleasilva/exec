8c4bc9dee46e8aab037a1f60ea4de80ac8f17bb7
Alexandre <aleasilva@yahoo.com.br>
2013-09-24 21:24:09 -0300

    Mostrando ordem musculo com opção de alteração.

--- app/models/musculo.rb
+++ app/models/musculo.rb
@@ -3,6 +3,7 @@ class Musculo < ActiveRecord::Base
   #Relacionamento com as atividades
   has_many :exercicios, dependent: :destroy
   has_many :atividades, through: :exercicios
+  accepts_nested_attributes_for :atividades
   
   #Relacionamento com os treinos.
   has_many :ordemmusculotreinos, dependent: :destroy
--- app/models/ordemmusculotreino.rb
+++ app/models/ordemmusculotreino.rb
@@ -1,4 +1,6 @@
 class Ordemmusculotreino < ActiveRecord::Base
   belongs_to :musculo
   belongs_to :treino
+  
+  accepts_nested_attributes_for :musculo
 end
