5cea4d3e9219b6fb71ffeab8fcbaeb2c3992e2dd
Alexandre <aleasilva@yahoo.com.br>
2013-09-30 22:03:00 -0300

    Passando os parametros para o Javascript.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,16 +23,16 @@ module ApplicationHelper
       #
       #link_to_function(name, h("add_fields2(this)"))
       #link_to_function "Greeting", "alert('Ola mundo!')", :class => "nav_link"      
-      link_to_function(name, h("add_fields2('x','x')"))
+      link_to "Greeting", '#', onclick: "add_fields('Hello world!','xxxx','ddddd')";
   end
   
   # generates add fields on a dynamic form
-  def link_to_add_fields3(name, f, association, locals={})  
-    new_object = f.object.class.reflect_on_association(association).klass.new  
-    fields = f.fields_for(association, new_object,:child_index => "new_#{association}") do |builder|  
-      render(association.to_s.singularize + "_fields", locals.merge!(:f => builder))  
-    end  
-  
-    link_to(name, "#", :class => "dynamic_add", 'data-association' => "#{association}", 'data-content' => "#{fields}")
-  end    
+  def link_to_add_fields3(name, f, association)
+    new_object = f.object.send(association).klass.new
+    id = new_object.object_id
+    fields = f.fields_for(association, new_object, child_index: id) do |builder|
+      render(association.to_s.singularize + "_fields", f: builder)
+    end
+    link_to(name, '#', class: "add_fields", data: {id: id, fields: fields.gsub("\n", "")})
+  end   
 end
