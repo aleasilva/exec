ca452fd08b2a8ce845ddb242d33f479213a55c3c
Alexandre <aleasilva@yahoo.com.br>
2013-10-11 21:10:45 -0300

    Incluindo bibliotecas wice_grid.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,14 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
-function remove_fields(link) {
-  $(link).previous("input[type=hidden]").value = "1";
-  $(link).up(".fields").hide();
-}
-
-function add_fields(link,association,content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + association, "g");
-  
-  $(link).up().insert({before: content.replace(regexp,new_id)});  
-}
+//= require jquery.ui.all
+//= require jquery.ui.datepicker
+//= require wice_grid
+//= require turbolinks
---
+++ app/assets/javascripts/lib.js
@@ -0,0 +1,25 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+$(function() {
+  $('#datepicker').datepicker();
+});
+function remove_fields(link) {
+  $(link).previous("input[type=hidden]").value = "1";
+  $(link).up(".fields").hide();
+}
+function add_fields(link,association,content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g");
+  
+  $(link).up().insert({before: content.replace(regexp,new_id)});  
+}
