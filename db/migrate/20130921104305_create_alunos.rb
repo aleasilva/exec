37a86602265c2d2b60b651d671172f3921b4e923
Alexandre <aleasilva@yahoo.com.br>
2013-10-24 11:36:26 -0200

    *Alteração na criação dos campos default do aluno.

--- db/migrate/20130921104305_create_alunos.rb
+++ db/migrate/20130921104305_create_alunos.rb
@@ -3,14 +3,14 @@ class CreateAlunos < ActiveRecord::Migration
     create_table :alunos do |t|
       t.string  :idAcademia   , :limit => 030, :default => ""
       t.string  :nome         , :limit => 060, :default => "Aluno" , :null => false
-      t.string  :medicamento  , :limit => 060, :default => ""
-      t.string  :doenca       , :limit => 060, :default => ""
-      t.string  :alergia      , :limit => 060, :default => ""
-      t.string  :dor          , :limit => 060, :default => ""
+      t.string  :medicamento  , :limit => 060, :default => "Não toma."
+      t.string  :doenca       , :limit => 060, :default => "Não possui."
+      t.string  :alergia      , :limit => 060, :default => "Não tem."
+      t.string  :dor          , :limit => 060, :default => "Não sente."
       t.string  :email        , :limit => 100, :default => ""
       t.text    :observacao
-      t.date    :nascimento
-      t.boolean :sexo         , :limit => 1 , :default => "F"     , :null => false
+      t.date    :nascimento   , :default => Date.today
+      t.string  :sexo         , :limit => 1 , :default => "f", :null => false
 
     end
   end
