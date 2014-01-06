8b756fd1af1432ec3a6fa2d327c090fcb3dc5516
Alexandre <aleasilva@yahoo.com.br>
2014-01-06 21:16:57 -0200

    Nao fazer o sign qdo o usuario for cadastrado,

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,2 +1,14 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-end
+class RegistrationsController < Devise::RegistrationsController
+  
+  def new
+    super
+  end
+
+  def create
+    # add custom create logic here
+  end
+
+  def update
+    super
+  end
+end 
