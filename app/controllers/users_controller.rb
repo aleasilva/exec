9476ea0f07ab77d1166dc1abf77e4bc52fd3b422
unknown <92038212@.claro.com.br>
2014-01-10 11:28:29 -0200

    Gravando alteracao dos campos de usuarios.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,33 @@ class UsersController < ApplicationController
   def change
     @user = User.find(params[:format])
     Rails.logger.info("CHANGE**********Registration Controller ****")    
-  end    
+  end
+  
+  
+  # PUT /resource
+  # We need to use a copy of the resource because we don't want to change
+  # the current user in place.
+  def update
+
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      # Sign in the user by passing validation in case his password changed
+      sign_in @user, :bypass => true
+      redirect_to root_path
+    else
+      render "edit"
+    end
+    
+  end
+  
+   # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+  # https://github.com/plataformatec/devise/wiki/How-To%3a-Allow-users-to-edit-their-account-without-providing-a-password
+  def needs_password?(user, params)
+    params[:password].present?
+  end
     
 end
