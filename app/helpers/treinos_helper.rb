66e5665fef7ffb2163f9e534537d21312c3842db
Alexandre <aleasilva@yahoo.com.br>
2015-06-14 23:28:32 -0300

    Uso de togle
    Uso de togle nos forms adicionados legendas no checkbox.

--- app/helpers/treinos_helper.rb
+++ app/helpers/treinos_helper.rb
@@ -1,5 +1,24 @@
 module TreinosHelper
+
+  #Indica se um treino pertence a uma atividade
   def seekAtividade(atividade)
     eof = @treino.atividades.include?(atividade)
   end
+
+  #Cria uma barra para identficacao dos Exercicios
+  def insCabExercicio()
+    content_tag(:div,content_tag(:a, "Exercicios", class: ["list-group-item list-group-item-success"]), class: ["lst-group"])
+    #content_tag(:div, "Exercicios", class: ["borderGreen"])
+  end
+
+
+  #Controla a quebra da linha da tabela de selecao de Exercicios
+  def ctlQuebraLinha(linha)
+    linha=linha +1
+    if linha % 4 == 0
+      content_tag(:div)
+      content_tag(:div, "Exercicios", class: ["row"])
+    end
+  end
+
 end
