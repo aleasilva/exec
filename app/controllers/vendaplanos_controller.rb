52be30e1a172d7b6080dab3f0903e7ebb290d9fe
Alexandre <aleasilva@yahoo.com.br>
2015-07-29 22:47:11 -0300

    Criação das parcelas dos planos
    Criação das parcelas dos planos.

--- app/controllers/vendaplanos_controller.rb
+++ app/controllers/vendaplanos_controller.rb
@@ -28,16 +28,45 @@ class VendaplanosController < ApplicationController
   # POST /vendaplanos
   def create
     @vendaplano = Vendaplano.new(vendaplano_params)
-    @vendaplano.vendidoPor = current_user.name
     qtdParcelas = @vendaplano.tabelaplano.qtdMaxParcela
 
-    byebug
+    begin
+      @vendaplano.transaction do
+        #
+        for iParcela in 1..qtdParcelas
 
-    if @vendaplano.save
-      redirect_to @vendaplano, notice: 'Vendaplano was successfully created.'
-    else
+          #Gera os planos para os proximos meses.
+          if iParcela == 1
+            @vendaplano.dtPagto = @vendaplano.dtVenda
+            @vendaplano.dtBaixa = @vendaplano.dtVenda
+            @vendaplano.recebidoPor = current_user.name
+          else
+            @vendaplano = criaParcela(@vendaplano)
+          end
+
+          @vendaplano.vendidoPor = current_user.name
+          @vendaplano.nuParcela = iParcela
+          @vendaplano.nuDia = @vendaplano.dtPagto.mday
+          @vendaplano.save
+
+        end
+        redirect_to @vendaplano, notice: 'Registros de vendas criados.'
+
+      end
+    rescue ActiveRecord::RecordInvalid => invalid
       render :new
     end
+  end
+
+  #Cria uma nova parcela
+  def criaParcela(parcelaAnterior)
+
+    novaParcela  = Vendaplano.new(vendaplano_params)
+    novaParcela.dtBaixa = nil
+    novaParcela.recebidoPor = nil
+    novaParcela.dtPagto = parcelaAnterior.dtPagto + 30
+
+    return novaParcela
   end
 
   # PATCH/PUT /vendaplanos/1
