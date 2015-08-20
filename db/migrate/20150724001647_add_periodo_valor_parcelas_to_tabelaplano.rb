2b5b1bffb406d8988611cf15d0a160aca37517db
Alexandre <aleasilva@yahoo.com.br>
2015-08-20 19:32:25 -0300

    Alteracao do tipo dos dados

--- db/migrate/20150724001647_add_periodo_valor_parcelas_to_tabelaplano.rb
+++ db/migrate/20150724001647_add_periodo_valor_parcelas_to_tabelaplano.rb
@@ -1,7 +1,7 @@
 class AddPeriodoValorParcelasToTabelaplano < ActiveRecord::Migration
   def change
     add_column :tabelaplanos, :periodo, :string
-    add_column :tabelaplanos, :valor, :double
+    add_column :tabelaplanos, :valor, :float
     add_column :tabelaplanos, :parcelas, :integer
   end
 end
