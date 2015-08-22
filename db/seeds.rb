ef3cbe7229dbd49af891e56d1148264f4d761a9c
Alexandre <aleasilva@yahoo.com.br>
2015-08-22 17:43:37 -0300

    Atualizacao controle acessos
    Atualizacao controle de acessos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#Populando os dias da semana
 diaSemanaList = [
   [ "Domingo", "dom",1 ],
   [ "Segunda", "seg",2 ],
@@ -17,4 +19,32 @@ diaSemanaList = [
 Diasemana.delete_all
 diaSemanaList.each do |nome, nomered, numDia|
   Diasemana.create( nome: nome, nomeabreviado: nomered, simbnumberal: numDia)
+end
+
+#Populando os grupos
+gruposAcessoList = [
+  ["SYSTEM_ADMIN", "Administrador do sistema"],
+  ["SUPER_USER","Um super usuario nao deve acessar as configuracoes do sistema"],
+  ["VENDA_PLANO","Acessa a area de venda de planos"],
+  ["TREINOS","Acessa a area de treinos"]
+]
+
+Group.delete_all
+gruposAcessoList.each do |nome, descricao|
+  Group.create( nome: nome, descricao: descricao)
+end
+
+#Populando as regras
+regrasList = [
+
+  ["READ","ALL","READ","Ler os dados de todas as tabelas"],
+  ["WRITE","ALL","WRITE","Gravar os dados de todas as tabelas"],
+  ["DELETE","ALL","DELETE","Excluir os dados de todas as tabelas"]
+
+]
+
+Rule.delete_all
+regrasList.each do |nome, entidade, regra, descricao|
+  Rule.create( nome: nome, entidade: entidade, regra: regra, descricao: descricao)
+  #Rule.create (nome: nome, entidade: entidade, regra: regra, descricao:descricao)
 end
